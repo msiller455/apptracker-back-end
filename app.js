4078346af1a09bc6ac62ffe0f9e0dafbf9e9a111
Michael <msiller455@gmail.com>
2019-05-22 16:26:31 -0700

    adds login and logout routes

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const session = require('express-session')
 const bodyParser = require('body-parser')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var authRouter = require('./routes/auth')
 
 var app = express();
 
@@ -24,9 +26,17 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(session({
+  secret: 'coderguy420',
+  cookie: { maxAge: 60000 },
+  resave: false,
+  saveUninitialized: false
+}))
+
 // Routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/auth', authRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
